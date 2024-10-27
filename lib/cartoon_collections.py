22bbe391324788db98a84523f48f3d74be96321b
Jose Soto <jsoto7087@gmail.com>
2024-10-26 21:47:31 -0400

    lab completed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,35 @@
-def roll_call_dwarves():
-    pass
+CHESSE = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def roll_call_dwarves(staff):
 
-def find_the_cheese():
-    pass
+    for index in range(len(staff)):
+        print(f'{index+1}. {staff[index]}')
+
+
+def summon_captain_planet(calls):
+
+    call_list = []
+    for call in calls: 
+        calls_string = f'{call.capitalize()}' + '!'
+        call_list.append(calls_string)
+
+    return call_list
+
+def long_planeteer_calls(calls):
+    
+    for call in calls:
+        if len(call) > 4:
+            return True
+        
+    return False
+
+
+def find_the_cheese(foods):
+    
+    for food in foods: 
+        if food in CHESSE:
+            return food
+    
+    return None
+    
